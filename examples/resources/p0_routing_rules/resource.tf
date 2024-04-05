542f65f9543034281d88075892abebb023bf0aec
Nathan Brahms <nbrahms@users.noreply.github.com>
2024-04-05 09:39:38 -0700

    Create P0 terraform provider (#1)
    This:
    - Sets up licensing for MPLv2
    - Creates a P0 Terraform provider
    - Adds support for controlling routing rules using this provider

---
+++ examples/resources/p0_routing_rules/resource.tf
@@ -0,0 +1,28 @@
+resource "p0_routing_rules" "example" {
+  rule {
+    requestor {
+      type      = "group"
+      directory = "okta"
+      id        = "00abcdefghijklmno697"
+      label     = "AWS Developers"
+    }
+    resource {
+      type    = "integration"
+      service = "aws"
+      filters = {
+        "tag" = {
+          effect  = "keep"
+          key     = "p0_grantable"
+          pattern = "1|true"
+        }
+      }
+    }
+    approval {
+      type        = "auto"
+      integration = "pagerduty"
+      options = {
+        require_reason = true
+      }
+    }
+  }
+}
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,11 +1,13 @@
-// Copyright (c) HashiCorp, Inc.
+// Copyright (c) HashiCorp, Inc. and P0 Security, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
 package provider
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/function"
@@ -13,80 +15,103 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"github.com/p0-security/terraform-provider-p0/internal"
+	"github.com/p0-security/terraform-provider-p0/internal/provider/resources"
 )
 
-// Ensure ScaffoldingProvider satisfies various provider interfaces.
-var _ provider.Provider = &ScaffoldingProvider{}
-var _ provider.ProviderWithFunctions = &ScaffoldingProvider{}
+// Ensure P0Provider satisfies various provider interfaces.
+var _ provider.Provider = &P0Provider{}
+var _ provider.ProviderWithFunctions = &P0Provider{}
 
-// ScaffoldingProvider defines the provider implementation.
-type ScaffoldingProvider struct {
+// P0Provider defines the provider implementation.
+type P0Provider struct {
 	// version is set to the provider version on release, "dev" when the
 	// provider is built and ran locally, and "test" when running acceptance
 	// testing.
 	version string
 }
 
-// ScaffoldingProviderModel describes the provider data model.
-type ScaffoldingProviderModel struct {
-	Endpoint types.String `tfsdk:"endpoint"`
+// P0ProviderModel describes the provider data model.
+type P0ProviderModel struct {
+	Org types.String `tfsdk:"org"`
 }
 
-func (p *ScaffoldingProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
-	resp.TypeName = "scaffolding"
+func (p *P0Provider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "p0"
 	resp.Version = p.version
 }
 
-func (p *ScaffoldingProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+func (p *P0Provider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
 	resp.Schema = schema.Schema{
+		MarkdownDescription: "Configures a P0 organization. Requires a P0 account. Go to https://p0.app to create an account.",
 		Attributes: map[string]schema.Attribute{
-			"endpoint": schema.StringAttribute{
-				MarkdownDescription: "Example provider attribute",
-				Optional:            true,
+			"org": schema.StringAttribute{
+				MarkdownDescription: "Your P0 organization identifier.",
+				Required:            true,
 			},
 		},
 	}
 }
 
-func (p *ScaffoldingProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
-	var data ScaffoldingProviderModel
+func (p *P0Provider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var model P0ProviderModel
 
-	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	resp.Diagnostics.Append(req.Config.Get(ctx, &model)...)
+
+	if model.Org.IsUnknown() {
+		resp.Diagnostics.AddError(
+			"P0 organization identifier is required",
+			"This is the identifier you use when logging in to https://p0.app.",
+		)
+	}
+
+	api_token := os.Getenv("P0_API_TOKEN")
+	if api_token == "" {
+		resp.Diagnostics.AddError(
+			"No P0_API_TOKEN environment variable",
+			fmt.Sprintf(
+				"A P0 API token is required to use the P0 Terraform provider. To create a token, navigate to https://p0.app/o/%s/settings. Pass your token by setting it in the P0_API_TOKEN environment variable.",
+				model.Org.ValueString(),
+			),
+		)
+	}
+
+	p0_host := os.Getenv("P0_HOST")
+	if p0_host == "" {
+		p0_host = "https://api.p0.app"
+	}
 
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Configuration values are now available.
-	// if data.Endpoint.IsNull() { /* ... */ }
-
-	// Example client configuration for data sources and resources
-	client := http.DefaultClient
-	resp.DataSourceData = client
-	resp.ResourceData = client
+	data := internal.P0ProviderData{
+		Authentication: fmt.Sprintf("Bearer %s", api_token),
+		Client:         http.DefaultClient,
+		BaseUrl:        fmt.Sprintf("%s/o/%s", p0_host, model.Org.ValueString()),
+	}
+	resp.DataSourceData = data
+	resp.ResourceData = data
 }
 
-func (p *ScaffoldingProvider) Resources(ctx context.Context) []func() resource.Resource {
+func (p *P0Provider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
-		NewExampleResource,
+		resources.NewRoutingRules,
 	}
 }
 
-func (p *ScaffoldingProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
-	return []func() datasource.DataSource{
-		NewExampleDataSource,
-	}
+func (p *P0Provider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{}
 }
 
-func (p *ScaffoldingProvider) Functions(ctx context.Context) []func() function.Function {
-	return []func() function.Function{
-		NewExampleFunction,
-	}
+func (p *P0Provider) Functions(ctx context.Context) []func() function.Function {
+	return []func() function.Function{}
 }
 
 func New(version string) func() provider.Provider {
 	return func() provider.Provider {
-		return &ScaffoldingProvider{
+		return &P0Provider{
 			version: version,
 		}
 	}
