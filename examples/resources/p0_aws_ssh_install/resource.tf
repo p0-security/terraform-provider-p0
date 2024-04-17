e47f904a1ba4d505f84ecbc2f47236c6be5a7980
Miguel Campos <miguel.campos@p0.dev>
2024-04-17 16:23:26 -0700

    simpler example

--- examples/resources/p0_aws_ssh_install/resource.tf
+++ examples/resources/p0_aws_ssh_install/resource.tf
@@ -1,5 +1,4 @@
 resource "p0_aws_ssh_install" "example" {
-  account_id = p0_aws_staged.example.id
-  depends_on = [p0_aws_staged.example]
+  account_id = "123456789012"
   group_key  = "vegetables"
 }
