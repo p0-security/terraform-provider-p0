c5e78081a31b64c87633502195d5dd03ca80a97c
Dominick Hing <dominick.hing1024@gmail.com>
2025-01-24 12:14:51 -0800

    Undo some specing changes

--- internal/web.go
+++ internal/web.go
@@ -1,5 +1,6 @@
 // Copyright (c) 2024 P0 Security, Inc
 // SPDX-License-Identifier: MPL-2.0
+
 package internal
 
 import (
@@ -22,20 +23,22 @@ type P0ProviderData struct {
 
 func (data *P0ProviderData) Do(req *http.Request, responseJson any) (*http.Response, error) {
 	req.Header.Add("Accept", "application/json")
-
 	resp, errDo := data.Client.Do(req)
 	if errDo != nil {
 		return resp, errDo
 	}
 	defer resp.Body.Close()
+
 	body, readErr := io.ReadAll(resp.Body)
 	if readErr != nil {
 		return resp, readErr
 	}
+
 	parseErr := json.Unmarshal(body, &responseJson)
 	if parseErr != nil {
 		return resp, parseErr
 	}
+
 	// If the response contains "error", throw that here
 	var generic map[string]any
 	genericErr := json.Unmarshal(body, &generic)
@@ -53,6 +56,7 @@ func (data *P0ProviderData) Do(req *http.Request, responseJson any) (*http.Respo
 		}
 		return resp, fmt.Errorf("%s: %s", resp.Status, errorText)
 	}
+
 	return resp, nil
 }
 
@@ -65,19 +69,18 @@ func (data *P0ProviderData) Get(path string, responseJson any) (*http.Response, 
 	}
 	return data.Do(req, responseJson)
 }
-
 func (data *P0ProviderData) Delete(path string) (*http.Response, error) {
 	req, errNew := http.NewRequest("DELETE", fmt.Sprintf("%s/%s", data.BaseUrl, path), nil)
 	req.Header.Add("Authorization", data.Authentication)
 	if errNew != nil {
 		return nil, errNew
 	}
-
 	resp, errDo := data.Client.Do(req)
 	if errDo != nil {
 		return resp, errDo
 	}
 	defer resp.Body.Close()
+
 	// Endpoints should not return content
 	// TODO: Render actual error
 	if resp.StatusCode != 204 {
@@ -85,15 +88,12 @@ func (data *P0ProviderData) Delete(path string) (*http.Response, error) {
 	}
 	return resp, nil
 }
-
 func (data *P0ProviderData) doBody(method string, path string, requestJson any, responseJson any) (*http.Response, error) {
 	buf, marshalErr := json.Marshal(&requestJson)
 	if marshalErr != nil {
 		return nil, marshalErr
 	}
-
 	reader := bytes.NewReader(buf)
-
 	req, errNew := http.NewRequest(method, fmt.Sprintf("%s/%s", data.BaseUrl, path), reader)
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Authorization", data.Authentication)
@@ -103,11 +103,9 @@ func (data *P0ProviderData) doBody(method string, path string, requestJson any, 
 	}
 	return data.Do(req, responseJson)
 }
-
 func (data *P0ProviderData) Post(path string, requestJson any, responseJson any) (*http.Response, error) {
 	return data.doBody("POST", path, requestJson, responseJson)
 }
-
 func (data *P0ProviderData) Put(path string, requestJson any, responseJson any) (*http.Response, error) {
 	return data.doBody("PUT", path, requestJson, responseJson)
 }
