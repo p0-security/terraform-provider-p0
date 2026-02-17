23e7d9006535806496a8e8cf5e414acc3e2b41eb
Gus Boling <gus.boling@p0.dev>
2026-02-16 22:23:06 -0800

    Fixing example for k8s staged provider, regenerate docs

--- examples/resources/p0_eks_kubernetes_staged/resource.tf
+++ examples/resources/p0_eks_kubernetes_staged/resource.tf
@@ -1,8 +1,6 @@
 resource "p0_kubernetes_staged" "test" {
-  id                    = "tf-staged-test-cluster"
-  connectivity_type     = "proxy"
-  hosting_type          = "aws"
+  id                    = "my-eks-cluster"
   cluster_arn           = "arn:aws:eks:us-west-2:123456789101:cluster/my-eks-cluster"
-  cluster_endpoint      = "https://asdfgdfsafasdafsafadfasdfasd.gr7.us-west-2.eks.amazonaws.com"
+  cluster_endpoint      = "https://ABC123XYC211242IAM.gr7.us-west-2.eks.amazonaws.com"
   certificate_authority = "ABIGBASE64CERTSTRING"
 }
