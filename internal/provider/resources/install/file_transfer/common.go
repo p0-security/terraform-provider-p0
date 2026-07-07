1903ccc468b1ce9cda3199f0f2409f51bf5bda93
Melissa Hoo <melissa.hoo@p0.dev>
2026-07-07 15:10:57 -0700

    remove validators no longer needed

--- internal/provider/resources/install/file_transfer/common.go
+++ internal/provider/resources/install/file_transfer/common.go
@@ -14,12 +14,3 @@ var Components = []string{installresources.IamWrite}
 // A DNS-style S3 bucket name: 3-63 characters, lowercase alphanumerics plus dots and
 // hyphens, beginning and ending with an alphanumeric. Mirrors the validation in the P0 app.
 var BucketNameRegex = regexp.MustCompile(`^[a-z0-9][a-z0-9.-]{1,61}[a-z0-9]$`)
-
-// An AWS region identifier, e.g. us-east-1 or us-gov-east-1.
-var RegionRegex = regexp.MustCompile(`^[a-z]{2}(?:-[a-z0-9]+)+-\d+[a-z0-9]*$`)
-
-// The AWS partitions supported for file transfer.
-var Partitions = []string{"aws", "aws-us-gov", "aws-cn"}
-
-// The default AWS partition (commercial AWS).
-const DefaultPartition = "aws"
